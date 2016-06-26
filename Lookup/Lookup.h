0a8765153b7414832d17013982a33eb1518d2705
Chris Nevin <cjnevin@gmail.com>
2016-06-26 08:50:37 +0100

    Converted to framework.

---
+++ Lookup/Lookup.h
@@ -0,0 +1,19 @@
+//
+//  Lookup.h
+//  Lookup
+//
+//  Created by Chris Nevin on 26/06/2016.
+//  Copyright © 2016 CJNevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Lookup.
+FOUNDATION_EXPORT double LookupVersionNumber;
+
+//! Project version string for Lookup.
+FOUNDATION_EXPORT const unsigned char LookupVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Lookup/PublicHeader.h>
+
+
